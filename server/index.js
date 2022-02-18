754678d7e6a104312cca7aae09f8bf2e048e6fa6
Valde Ganov <valde.ganov@gmail.com>
2022-02-18 19:23:14 +0700

    Debuging and deployment

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,13 +14,16 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://valdeganov:valdeganov03@cluster0.wewlg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.get("/", (req, res) => {
+  res.send("Hello to Stock Web App API");
+});
 
+// const CONNECTION_URL =
+//   "mongodb+srv://valdeganov:valdeganov03@cluster0.wewlg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -28,4 +32,4 @@ mongoose
   )
   .catch((error) => console.log(error.message));
 
-//mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
